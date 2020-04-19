4ba71a8fc6db81c1d9c9a5f5bf1214a3ed093447
Noel Gaetan <gaetan.noel@viacesi.fr>
2020-04-20 01:09:35 +0200

    Fix error & add bootloader missing folder 	modified:   boards.txt 	new file:   bootloader/holyiot_nrf52840/holyiot_nrf52840_bootloader-0.3.2_s140_6.1.1.hex 	new file:   bootloader/holyiot_nrf52840/holyiot_nrf52840_bootloader-0.3.2_s140_6.1.1.zip 	modified:   variants/holyiot_nrf52840/variant.cpp 	modified:   variants/holyiot_nrf52840/variant.h

--- variants/holyiot_nrf52840/variant.h
+++ variants/holyiot_nrf52840/variant.h
@@ -37,14 +37,14 @@ extern "C"
 #endif // __cplusplus
 
 // Number of pins defined in PinDescription array
-#define PINS_COUNT           (48)
-#define NUM_DIGITAL_PINS     (48)
-#define NUM_ANALOG_INPUTS    (6)
+#define PINS_COUNT           (3)
+#define NUM_DIGITAL_PINS     (3)
+#define NUM_ANALOG_INPUTS    (0) // A6 is used for battery, A7 is analog reference
 #define NUM_ANALOG_OUTPUTS   (0)
 
 // LEDs
-#define PIN_LED1             (13)
-#define PIN_LED2             (14)
+#define PIN_LED1             (0)
+#define PIN_LED2             (1)
 
 #define LED_BUILTIN          PIN_LED1
 #define LED_CONN             PIN_LED2
@@ -57,82 +57,30 @@ extern "C"
 /*
  * Buttons
  */
-#define PIN_BUTTON1          11
-#define PIN_BUTTON2          12
-#define PIN_BUTTON3          24
-#define PIN_BUTTON4          25
+#define PIN_BUTTON1          2
 
+#define PIN_DFU              2
+
+// Buttons
 /*
- * Analog pins
- */
-#define PIN_A0               (3)
-#define PIN_A1               (4)
-#define PIN_A2               (28)
-#define PIN_A3               (29)
-#define PIN_A4               (30)
-#define PIN_A5               (31)
-#define PIN_A6               (0xff)
-#define PIN_A7               (0xff)
+#define PIN_BUTTON1             (2)
+#define PIN_BUTTON2             (3)
+#define PIN_BUTTON3             (4)
+#define PIN_BUTTON4             (5)
+*/
 
-static const uint8_t A0  = PIN_A0 ;
-static const uint8_t A1  = PIN_A1 ;
-static const uint8_t A2  = PIN_A2 ;
-static const uint8_t A3  = PIN_A3 ;
-static const uint8_t A4  = PIN_A4 ;
-static const uint8_t A5  = PIN_A5 ;
-static const uint8_t A6  = PIN_A6 ;
-static const uint8_t A7  = PIN_A7 ;
 #define ADC_RESOLUTION    14
 
-// Other pins
-#define PIN_AREF           (2)
-#define PIN_NFC1           (9)
-#define PIN_NFC2           (10)
+// Bad things happen if the serial pins aren't defined. Instead
+// define them with an invalid pin number
+// Serial
+#define PIN_SERIAL1_RX       (3)
+#define PIN_SERIAL1_TX       (3)
 
-static const uint8_t AREF = PIN_AREF;
 
-/*
- * Serial interfaces
- */
-
-// Arduino Header D0, D1
-#define PIN_SERIAL1_RX      (33) // P1.01
-#define PIN_SERIAL1_TX      (34) // P1.02
-
-// Connected to Jlink CDC
-#define PIN_SERIAL2_RX      (8)
-#define PIN_SERIAL2_TX      (6)
-
-/*
- * SPI Interfaces
- */
-#define SPI_INTERFACES_COUNT 1
-
-#define PIN_SPI_MISO         (46)
-#define PIN_SPI_MOSI         (45)
-#define PIN_SPI_SCK          (47)
-
-static const uint8_t SS   = 44 ;
-static const uint8_t MOSI = PIN_SPI_MOSI ;
-static const uint8_t MISO = PIN_SPI_MISO ;
-static const uint8_t SCK  = PIN_SPI_SCK ;
-
-/*
- * Wire Interfaces
- */
-#define WIRE_INTERFACES_COUNT 1
-
-#define PIN_WIRE_SDA         (26)
-#define PIN_WIRE_SCL         (27)
-
-// QSPI Pins
-#define PIN_QSPI_SCK         19
-#define PIN_QSPI_CS          17
-#define PIN_QSPI_IO0         20
-#define PIN_QSPI_IO1         21
-#define PIN_QSPI_IO2         22
-#define PIN_QSPI_IO3         23
-
+#ifdef __cplusplus
+}
+#endif
 
 /*----------------------------------------------------------------------------
  *        Arduino objects - C++ only
